19281f3962742e1e984867f81462f414c4e4d954
Joris van der Sluis <0946664@hr.nl>
2020-04-01 12:35:41 +0200

    Added working multiplier

--- ExcelColumnModifier.py
+++ ExcelColumnModifier.py
@@ -1,68 +1,56 @@
 import csv
 import numpy as np
 from re import sub
-startValue = 10
-columnName = input('What is de header of the column you want to change? ')
-shouldModifyAllRows = input('Do you want to modify all rows? y/n?  ') 
+# startValue = 10
+# columnName = input('What is de header of the column you want to change? ')
+# shouldModifyAllRows = input('Do you want to modify all rows? y/n?  ') 
 
-if shouldModifyAllRows == 'y':
-    start = 0
-    end = 10
-else:
-    start = input('What is the first row number you would like to modify? ')
-    end = input('What is the last row number you would like to modify? ')
+# if shouldModifyAllRows == 'y':
+#     start = 0
+#     end = 10
+# else:
+#     start = input('What is the first row number you would like to modify? ')
+#     end = input('What is the last row number you would like to modify? ')
 
-valueType = input('What is the type of values you want to modify? number or text? n/t? ')
-if valueType == 'n':  
-   modification = input('How would you like to modify? combine(c) subtract(s) multiply(m) or devide(d) ? ')
-else: 
-    modification = input('Would you like to append(a) or replace(r) the value ? ')
+# valueType = input('What is the type of values you want to modify? number or text? n/t? ')
+# if valueType == 'n':  
+#    modification = input('How would you like to modify? combine(c) subtract(s) multiply(m) or devide(d) ? ')
+# else: 
+#     modification = input('Would you like to append(a) or replace(r) the value ? ')
 
-value = input('What is the value you would like to use? ')
+# value = input('What is the value you would like to use? ')
 
-if modification == 'c':
-    print(startValue + float(value))
-elif modification == 's':
-    print(startValue - float(value))
-elif modification == 'm':
-    print(startValue * float(value))
-elif modification == 'd':
-    print(startValue / float(value))
-elif modification == 'a':
-    print( str(startValue) + value)
-else: 
-    print(value)
+# if modification == 'c':
+#     print(startValue + float(value))
+# elif modification == 's':
+#     print(startValue - float(value))
+# elif modification == 'm':
+#     print(startValue * float(value))
+# elif modification == 'd':
+#     print(startValue / float(value))
+# elif modification == 'a':
+#     print( str(startValue) + value)
+# else: 
+#     print(value)
 
-
-# with open(r'D:\Users\jjori\Documents\CeVeDe\csv2\test2.csv', 'r+') as csv_file:
-#     priceString = 'Variant Price'
-#     comparePriceString = 'Variant Compare At Price'
-#     reader = csv.reader(csv_file)
-#     writer = csv.writer(csv_file)
-#     # dict_reader = csv.DictReader(csv_file)
-#     # fieldnames = dict_reader.fieldnames
-#     # dict_writer = csv.DictWriter(csv_file, fieldnames=[fieldnames])
-#     headers =  next(reader)
-#     priceIndex = headers.index(priceString)
-
-#     for row in reader:
-#        print(len(row[0]))
-#        if len(row[0]) == 0:
-#            break
-#        price = row[priceIndex]   
-#        print(price)
-#        writer.writerow(row)
-    #    if not ''.join(row).strip():
-            # price = row[priceIndex]
-            # price = (float(price[4:-1]) * 1.04)
-            # price = row[priceString]
-            # comparePrice = row[comparePriceString]
-            # print(price, comparePrice)
-            # price = float(sub(r'[^\d.]', '',price[4:-1])) * 1.04
-            # print(price, comparePrice)
-            #row[priceString] = price
-     
+with open('persons.csv', 'w', encoding='utf-8-sig') as csvfile2:
+        dict_writer = csv.writer(csvfile2, lineterminator = '\n')
+        with open(r'D:\Users\jjori\Documents\CeVeDe\csv2\test2.csv', 'r', encoding='utf-8-sig') as csv_file:
+            priceString = 'Variant Price'
+            comparePriceString = 'Variant Compare At Price'
+            reader = csv.reader(csv_file)
         
-        # dict_writer.writerow(row)
-    # print(PriceColumnIndex)
-# print(ComparePriceColumnIndex)
+            # dict_reader = csv.DictReader(csv_file)
+            # fieldnames = dict_reader.fieldnames
+
+            headers =  next(reader)
+            print(headers)
+            priceIndex = headers.index(priceString)
+            compareIndex = headers.index(comparePriceString)
+            dict_writer.writerow(headers)
+            for row in reader:
+                print(len(row))
+                
+                row[priceIndex] = round(float(sub(r'[^\d.]', '',row[priceIndex])) * 1.04, 2)
+                row[compareIndex] = round(float(sub(r'[^\d.]', '',row[compareIndex])) * 1.04, 2)
+                dict_writer.writerow(row)
