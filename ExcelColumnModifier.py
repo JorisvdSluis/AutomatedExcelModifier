e0066e51081e07eae617d57acc5819c489e902b6
jjori <jjori8@hotmail.com>
2022-12-28 17:42:38 +0100

    read files from folder

--- ExcelColumnModifier.py
+++ ExcelColumnModifier.py
@@ -33,27 +33,35 @@ from re import sub
 # else: 
 #     print(value)
 
-with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 07-2022\strada hybrid jaga kleur met extra types (wel javascript).csv', 'w', encoding='utf-8-sig') as csvfile2:
-        dict_writer = csv.writer(csvfile2, lineterminator = '\n')
-        with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 02-2022\strada hybrid jaga kleur met extra types (wel javascript).csv', 'r', encoding='utf-8-sig') as csv_file:
-            priceString = 'Variant Price'
-            comparePriceString = 'Variant Compare At Price'
+#with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 07-2022\strada hybrid jaga kleur met extra types (wel javascript).csv', 'w', encoding='utf-8-sig') as csvfile2:
+#        dict_writer = csv.writer(csvfile2, lineterminator = '\n')
+#        with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 02-2022\strada hybrid jaga kleur met extra types (wel javascript).csv', 'r', encoding='utf-8-sig') as csv_file:
+#            priceString = 'Variant Price'
+#            comparePriceString = 'Variant Compare At Price'
+#            reader = csv.reader(csv_file)
+#        
+#            # dict_reader = csv.DictReader(csv_file)
+#            # fieldnames = dict_reader.fieldnames
+#
+#            headers =  next(reader)
+#            print(headers)
+#            priceIndex = headers.index(priceString)
+#            compareIndex = headers.index(comparePriceString)
+#            dict_writer.writerow(headers)
+#            for row in reader:
+#                print(len(row))
+#                
+#                row[priceIndex] = round(float(sub(r'[^\d.]', '',row[priceIndex])) * 1.045, 2)
+#                row[compareIndex] = round(float(sub(r'[^\d.]', '',row[compareIndex])) * 1.045, 2)
+#                dict_writer.writerow(row)
+
+
+files = ["mini wand jaga kleur.csv", "mini wand jaga kleur.csv", "mini wand standaard kleur.csv", "strada hybrid jaga kleur.csv", "Strada standaard s.csv", "Strada standaard s.csv", "Strada standaard s.csv", "strada twin jaga kleur.csv","strada twin s.csv", "Tempo totaal.csv","vertiga totaal.csv"]
+
+with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 01-2023\jaga.csv', 'w', encoding='utf-8-sig') as origin:
+      dict_writer = csv.writer(origin, lineterminator = '\n')
+      for fileName in files:
+        with open(r'C:\Users\jjori\Documents\CeVeDe\prijslijst 01-2023\{}'.format(fileName), 'r', encoding='utf-8-sig') as csv_file:
             reader = csv.reader(csv_file)
-        
-            # dict_reader = csv.DictReader(csv_file)
-            # fieldnames = dict_reader.fieldnames
-
-            headers =  next(reader)
-            print(headers)
-            priceIndex = headers.index(priceString)
-            compareIndex = headers.index(comparePriceString)
-            dict_writer.writerow(headers)
             for row in reader:
-                print(len(row))
-                
-                row[priceIndex] = round(float(sub(r'[^\d.]', '',row[priceIndex])) * 1.045, 2)
-                row[compareIndex] = round(float(sub(r'[^\d.]', '',row[compareIndex])) * 1.045, 2)
-                dict_writer.writerow(row)
-
-
-# files = ["mini wand jaga kleur.csv", "mini wand jaga kleur.csv", "mini wand standaard kleur.csv", "strada hybrid jaga kleur.csv", "Strada standaard s.csv", "Strada standaard s.csv", "Strada standaard s.csv", "strada twin jaga kleur.csv","strada twin s.csv", "Tempo totaal.csv","vertiga totaal.csv"]
+                print(fileName)
